466ee8b3bf0d6226b53902e61a48fa973ce1de4e
woorim960 <woorimprog@gmail.com>
2022-10-12 22:58:22 +0900

    style: update console message for detail

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -6,7 +6,8 @@ const fs = require("fs");
 const projectName = process.argv[2] || "dev-portfolio";
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const GIT_REPO = "https://github.com/modern-agile-team/create-dev-portfolio.git";
+const GIT_REPO =
+  "https://github.com/modern-agile-team/create-dev-portfolio.git";
 
 if (projectName !== ".") {
   try {
@@ -14,7 +15,9 @@ if (projectName !== ".") {
   } catch (err) {
     if (err.code === "EEXIST") {
       console.log(projectName);
-      console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
+      console.log(
+        `The file ${projectName} already exist in the current directory, please give it another name.`
+      );
     } else {
       console.log(err);
     }
@@ -35,37 +38,39 @@ const checkIsDependencyExist = (system) => {
 async function main() {
   try {
     checkIsDependencyExist("docker");
+    checkIsDependencyExist("docker-compose");
     checkIsDependencyExist("node");
     checkIsDependencyExist("npm");
     checkIsDependencyExist("bash");
-    checkIsDependencyExist("docker-compose");
 
     console.log("Cloning dev-portfolio...");
-    execSync(`git clone --recursive --depth 1 ${GIT_REPO} ${projectPath}`); // 우리의 보일러 플레이트를 clone!
+    execSync(`git clone --depth 1 ${GIT_REPO} ${projectPath}`); // 우리의 보일러 플레이트를 clone!
 
     if (projectName !== ".") {
       process.chdir(`${projectPath}`); // cd입니다 clone을 마친 후 projectPath로 진입
     }
-
-    console.log("Downloading submodules...");
-    execSync(`git submodule update --init --remote --merge`); // 서브모듈 install!
 
     console.log("Installing dependencies...");
     execSync("npm run bootstrap"); // package.json에 있는 의존성 설치
 
     // 이제 보일러플레이트 git과 관련된 내용 제거
     console.log("Removing useless files...");
+    execSync("npm install -g glob"); // 윈도우에서 npx rimraf를 하려면 glob 필요.
     execSync("npx rimraf ./.git");
     execSync("npx rimraf ./client/.github");
 
     if (requiredDependencies.length > 0) {
       console.log("Required dependencies below\n");
       for (const dependency of requiredDependencies) {
-        console.log(`${dependency} is required\n`);
+        console.log(`${dependency} is required!`);
       }
     } else {
-      console.log("The installation is done, this is ready to use!\n");
-      console.log("Start dev-portfilio by typing 'npm run start:all' in your terminal!\n");
+      console.log("Installation is done, this is ready to use.\n");
+      console.log(
+        "Start dev-portfilio by typing the following command in your terminal!\n"
+      );
+      console.log("$ cd dev-portfolio or cd YOUR_APP_NAME");
+      console.log("$ npm run start:all\n");
     }
   } catch (error) {
     console.log(error);
